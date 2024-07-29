96f464061729e28c284a2b5fcd540ac1ab268e98
Max-Fabian Ksoll <maxfabian.ksoll@eudemonia-solutions.de>
2024-07-29 09:47:09 +0200

    model params

--- lightning_training_slurm.py
+++ lightning_training_slurm.py
@@ -39,7 +39,7 @@ def get_autoregression_step_epochs():
 
 
 def get_model_parameter():
-    return {"channels": 1024, "transformer_blocks": 40, "transformer_heads": 8}
+    return {"channels": 2048, "transformer_blocks": 20, "transformer_heads": 16}
 
 
 def get_opt_config():

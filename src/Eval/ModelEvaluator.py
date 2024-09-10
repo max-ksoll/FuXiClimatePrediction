d37e6fefe11babeb674b51c7319344bc5015d37e
Max-Fabian Ksoll <maxfabian.ksoll@eudemonia-solutions.de>
2024-09-10 21:43:21 +0200

    no src. import

--- src/Eval/ModelEvaluator.py
+++ src/Eval/ModelEvaluator.py
@@ -7,8 +7,8 @@ from matplotlib import pyplot as plt
 import cartopy
 import cartopy.crs as ccrs
 
-from src.Dataset.fuxi_dataset import FuXiDataset
-from src.PyModel.fuxi_ligthning import FuXi
+from ..Dataset.fuxi_dataset import FuXiDataset
+from ..PyModel.fuxi_ligthning import FuXi
 
 cartopy.config["pre_existing_data_dir"] = os.environ["CARTOPY_DIR"]
 

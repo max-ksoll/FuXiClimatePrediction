f9bca88851385b810ad8798ee4c48185980aa972
Max-Fabian Ksoll <maxfabian.ksoll@eudemonia-solutions.de>
2024-09-17 20:09:44 +0200

    sys import before use

--- src/Eval/ModelEvaluator.py
+++ src/Eval/ModelEvaluator.py
@@ -3,8 +3,6 @@ import os
 import sys
 from functools import lru_cache
 
-from src.Dataset.dimensions import LEVEL_VARIABLES, SURFACE_VARIABLES
-
 sys.path.append(os.environ["MODULE_PATH"])
 
 import cv2
@@ -16,6 +14,7 @@ import cartopy.crs as ccrs
 
 from src.Dataset.fuxi_dataset import FuXiDataset
 from src.PyModel.fuxi_ligthning import FuXi
+from src.Dataset.dimensions import LEVEL_VARIABLES, SURFACE_VARIABLES
 
 cartopy.config["pre_existing_data_dir"] = os.environ["CARTOPY_DIR"]
 logging.basicConfig(level=logging.DEBUG)

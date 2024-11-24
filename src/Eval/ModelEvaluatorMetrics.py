0dc42e26b60be1240d370745e2fbcd8a35e00b55
Max-Fabian Ksoll <maxfabian.ksoll@eudemonia-solutions.de>
2024-11-24 01:28:26 +0100

    target

--- src/Eval/ModelEvaluatorMetrics.py
+++ src/Eval/ModelEvaluatorMetrics.py
@@ -54,7 +54,7 @@ class ModelEvaluator:
             last_timestep = x[:, -1]
             mask = ~torch.isnan(last_timestep)
 
-            out_last_timestep = self.model(x)[:, -1]
+            out_last_timestep = self.model(x, None)[:, -1]
             error = last_timestep - out_last_timestep
 
             mae.append(float(torch.sum(torch.abs(error) * mask) / mask.sum()))
